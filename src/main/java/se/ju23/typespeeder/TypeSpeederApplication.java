233514055dbb0ed52f861d06b123268edf1f018a
MagNording <magnus_nording@msn.com>
2024-02-07 21:34:51 +0100

    Efter bråk med såväl Maven som .git gör jag bara något!

--- src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -2,12 +2,44 @@ package se.ju23.typespeeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TypeSpeederApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TypeSpeederApplication.class, args);
+        System.out.println(Arrays.toString(wordList()));
     }
 
+    @Bean
+    public static String[] wordList() {
+        return new String[] {
+            "hej",
+            "på",
+            "dig",
+            "hur",
+            "mår",
+            "du",
+            "jag",
+            "är",
+            "en",
+            "robot",
+            "som",
+            "skriver",
+            "snabbt",
+            "och",
+            "bra",
+            "på",
+            "att",
+            "skriva",
+            "ord",
+            "och",
+            "text",
+
+        };
+    }
 }
+
