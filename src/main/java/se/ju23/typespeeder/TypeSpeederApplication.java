71fa3624324ea2468d7c4204d865cd9b5dba48f9
Emelie Lind <emelie.lind@iths.se>
2024-02-08 15:26:29 +0100

    Added menu

--- src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -8,6 +8,49 @@ public class TypeSpeederApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TypeSpeederApplication.class, args);
+
+        System.out.println("Välkommen till TypeSpeeder!");
+
+        logIn();
+
+        int menuChoise = 0;
+        switch (menuChoise) {
+            case 0 -> logOut();
+            case 1 -> playGame();
+            case 2 -> showRankingList();
+            case 3 -> showNewsAndUpdates();
+            case 4 -> changeLanguage();
+            case 5 -> updateProfile();
+            default -> System.out.println("Felaktig inmatning, försök igen.");
+        }
     }
+
+    public static void logIn(){
+        System.out.println("Ange användarnamn och lösenord.");
+    }
+
+    public static void logOut(){
+    }
+
+    public static void playGame(){
+
+    }
+
+    public static void showRankingList(){
+
+    }
+
+    public static void showNewsAndUpdates(){
+
+    }
+
+    public static void changeLanguage(){
+
+    }
+
+    public static void updateProfile(){
+
+    }
+
 
 }
