a9ed8dec5294849215d804b219c4b8204ba48a5b
Magnus Nording <magnus.nording@iths.se>
2024-02-08 13:54:57 +0100

    adding ConsoleColor class of enums

---
+++ src/main/java/se/ju23/typespeeder/ui/ConsoleColor.java
@@ -0,0 +1,20 @@
+package se.ju23.typespeeder.ui;
+
+public enum ConsoleColor {
+    RESET("\u001B[0m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m");
+
+    private final String code;
+
+    ConsoleColor(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}
