23ec0bc303d03151247b32240b6e39f8a917f2ee
Estrid4Ever <johannes-sr@hotmail.com>
2024-02-09 11:30:05 +0100

    hej

---
+++ src/main/java/se/ju23/typespeeder/Runner.java
@@ -0,0 +1,13 @@
+package se.ju23.typespeeder;
+
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+
+@Component
+public class Runner implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -3,6 +3,8 @@ package se.ju23.typespeeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.Color;
+
 @SpringBootApplication
 public class TypeSpeederApplication {
 
