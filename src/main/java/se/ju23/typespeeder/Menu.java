06ad53865ffe0de9ccce88052768dbf07d6858d7
Emelie Jansson <emelie.jansson@iths.se>
2024-02-11 18:31:43 +0100

    added update 1

---
+++ src/main/java/se/ju23/typespeeder/Menu.java
@@ -0,0 +1,13 @@
+package se.ju23.typespeeder;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Menu {
+    public void displayMenu() {
+    }
+
+    public List<String> getMenuOptions() {
+        return null;
+    }
+}
