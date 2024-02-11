afc8a1ecc0578aa2f3c96bc8c21f74fea92bea0c
Emelie Lind <emelie.lind@iths.se>
2024-02-11 17:54:23 +0100

    Created class User and made the main menu

--- src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -9,5 +9,39 @@ public class TypeSpeederApplication {
     public static void main(String[] args) {
         SpringApplication.run(TypeSpeederApplication.class, args);
 
+        System.out.println("Välkommen till TypeSpeeder!");
+
+        User.logIn();
+
+        int menuChoise = 0;
+        switch (menuChoise) {
+            case 0 -> User.logOut();
+            case 1 -> playGame();
+            case 2 -> showRankingList();
+            case 3 -> showNewsAndUpdates();
+            case 4 -> changeLanguage();
+            case 5 -> User.updateProfile();
+            default -> System.out.println("Felaktig inmatning, försök igen.");
+        }
     }
+
+
+    public static void playGame(){
+
+    }
+
+    public static void showRankingList(){
+
+    }
+
+    public static void showNewsAndUpdates(){
+
+    }
+
+    public static void changeLanguage(){
+
+    }
+
+
+
 }
---
+++ src/main/java/se/ju23/typespeeder/User.java
@@ -0,0 +1,18 @@
+package se.ju23.typespeeder;
+
+import java.util.Optional;
+
+public class User {
+
+
+    public static void logIn() {
+
+    }
+
+    public static void logOut() {
+
+    }
+
+    public static void updateProfile() {
+    }
+}
