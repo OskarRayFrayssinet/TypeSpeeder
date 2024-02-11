cf309d646091fecab214eebf79f8772b96b5be61
Emelie Lind <emelie.lind@iths.se>
2024-02-11 19:05:07 +0100

    created interface MenuService and updated main menu

--- src/main/java/se/ju23/typespeeder/Menu.java
+++ src/main/java/se/ju23/typespeeder/Menu.java
@@ -1,11 +1,28 @@
 package se.ju23.typespeeder;
 
-import java.util.Collection;
 import java.util.List;
 
 public class Menu {
     List<String>MenuOptions;
     public void displayMenu() {
+
+        System.out.println("Välkommen till TypeSpeeder!");
+
+    }
+
+    public static void playGame(){
+
+    }
+
+    public static void showRankingList(){
+
+    }
+
+    public static void showNewsAndUpdates(){
+
+    }
+
+    public static void changeLanguage(){
 
     }
 
--- src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -11,42 +11,25 @@ public class TypeSpeederApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(TypeSpeederApplication.class, args);
 
-        System.out.println("Välkommen till TypeSpeeder!");
+
+    }
+    @Override
+    public void run(String... args) throws Exception {
 
         User.logIn();
 
         int menuChoice = 0;
         switch (menuChoice) {
             case 0 -> User.logOut();
-            case 1 -> playGame();
-            case 2 -> showRankingList();
-            case 3 -> showNewsAndUpdates();
-            case 4 -> changeLanguage();
+            case 1 -> Menu.playGame();
+            case 2 -> Menu.showRankingList();
+            case 3 -> Menu.showNewsAndUpdates();
+            case 4 -> Menu.changeLanguage();
             case 5 -> User.updateProfile();
             default -> System.out.println("Felaktig inmatning, försök igen.");
         }
     }
-    @Override
-    public void run(String... args) throws Exception {
 
-    }
-
-
-    public static void playGame(){
-
-    }
-
-    public static void showRankingList(){
-
-    }
-
-    public static void showNewsAndUpdates(){
-
-    }
-
-    public static void changeLanguage(){
-
-    }
 
 
 
