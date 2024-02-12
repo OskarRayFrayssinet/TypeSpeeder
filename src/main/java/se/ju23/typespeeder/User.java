30dd50e84ee7f4bb6fb6f0f6b77ed42d6c023ae9
Emelie Lind <emelie.lind@iths.se>
2024-02-12 13:30:00 +0100

    The 'User' class is annotated with @Entity to represent a user entity

--- src/main/java/se/ju23/typespeeder/User.java
+++ src/main/java/se/ju23/typespeeder/User.java
@@ -1,9 +1,30 @@
 package se.ju23.typespeeder;
 
-import java.util.Optional;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+
+@Entity
 public class User {
+    @Id
+    private String username;
+    private String password;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public static void logIn() {
 
