9b91f4bec3414ec09aa9791124677dfb8705957b
22aaan <nohh_goitom@live.se>
2024-02-16 12:46:20 +0100

    Testar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Scanner;
+
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
