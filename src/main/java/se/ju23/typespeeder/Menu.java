ad9db1098424253f7ac13ead0933f25e80c89298
Emelie Jansson <emelie.jansson@iths.se>
2024-02-19 14:15:50 +0100

    Modified checkSpelling and created checkOrder method in Menu

--- src/main/java/se/ju23/typespeeder/Menu.java
+++ src/main/java/se/ju23/typespeeder/Menu.java
@@ -24,9 +24,13 @@ public class Menu implements MenuService {
     private static Object LoggedInUser;
     private static ResourceBundle messages;
 
+    @Autowired
+    static void setUserService(UserService userService){
+        Menu.userService = userService;
+    }
 
     static List<String> MenuOptions = new ArrayList<>();
-    public static final String YELLOW = "\u001B[33m";
+    public static final String WHITE = "\u001B[37m";
     public static final String RESET = "\u001B[0m";
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
@@ -35,6 +39,7 @@ public class Menu implements MenuService {
     public static long startTime;
     public static String game;
     public static String colorWords;
+    public static List<String> redWords = new ArrayList<>();
 
 
     public static void displayMenu() throws IOException {
@@ -85,6 +90,7 @@ public class Menu implements MenuService {
         String password = input.nextLine();
 
 
+
         User user = userService.logIn(username, password);
 
         if (user != null) {
@@ -99,11 +105,6 @@ public class Menu implements MenuService {
     public static void logOut() {
         LoggedInUser = null;
         System.out.println("Du har loggats ut.");
-    }
-
-    @Autowired
-    static void setUserService(UserService userService) {
-        Menu.userService = userService;
     }
 
 
@@ -122,12 +123,13 @@ public class Menu implements MenuService {
         System.out.print("SKRIV HÄR --> ");
         game = input.nextLine();
         stopTimer = true;
-        correctSpell();
+        checkSpelling();
+        checkOrder();
 
     }
 
     public static void openTextFile() throws FileNotFoundException {
-
+        StringBuilder colorWordsBuilder = new StringBuilder();
         File file = new File("C:\\Users\\janss\\IdeaProjects\\Text2.txt");
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
@@ -135,15 +137,15 @@ public class Menu implements MenuService {
             String[] words = textFile.split(" ");
             for (String word : words) {
                 String color = Math.random() < 0.5 ? RED : GREEN;
-                colorWords = color + word + " ";
-                System.out.print(colorWords + RESET);
+                colorWordsBuilder.append(color).append(word).append(" ");
+                System.out.print(color + word + " " + RESET);
             }
             //System.out.print(RESET);
         }
+        colorWords = colorWordsBuilder.toString();
     }
-
-    public static void timer() {
-        Thread timer = new Thread(() -> {
+    public static void timer(){
+    Thread timer = new Thread(()->{
             startTime = System.currentTimeMillis();
             while (!stopTimer) {
                 try {
@@ -153,32 +155,40 @@ public class Menu implements MenuService {
                 }
             }
             long timeSeconds = (System.currentTimeMillis() - startTime) / 1000;
-            System.out.println("Din tid blev: " + timeSeconds + " sekunder");
+            System.out.println(RESET + "Din tid blev: " + timeSeconds + " sekunder");
         });
         timer.start();
     }
-
-    public static void correctSpell() {
-        List<String> redWords = new ArrayList<>();
-        String[] words = colorWords.split("\\s+");
+    public static void checkSpelling(){
+        String [] words = colorWords.split("\\s+");
         for (String word : words) {
-            if (word.contains(RED)) {
-                String[] red = word.split(RED);
-                redWords.add(red[1]);
+            if (word.startsWith(RED)) {
+                redWords.add(word.substring(RED.length()));
             }
         }
+        String [] gameList = game.split("\\s+");
         int countWords = 0;
-        for (String red : redWords) {
-            if (game.equals(red)) {
-                countWords++;
-
+        for (String list : gameList){
+            for(String red : redWords){
+                if (red.equals(list)){
+                    countWords++;
+                }
             }
         }
-        System.out.println("Antal rättstavade ord = " + countWords);
+        System.out.println(RESET + "Antal rättstavade ord = " + countWords);
     }
-
-    public static void rightOrder() {
-
+    public static void checkOrder(){
+        int countOrder = 0;
+        String[] gameList = game.split("\\s+");
+        int minWord = Math.min(redWords.size(), gameList.length);
+        for(int i = 0; i < minWord; i++){
+            if(gameList[i].equals(redWords.get(i))){
+                countOrder++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Antal ord i korrekt ordnind: " + countOrder);
     }
 
     public static void showRankingList() {
