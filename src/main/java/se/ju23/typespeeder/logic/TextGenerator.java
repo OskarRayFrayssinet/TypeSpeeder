0a57bdad50d14d789eebc40fe1d50703f971e2bc
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2024-02-20 20:22:08 +0100

    tagit bort överflödig kod

--- src/main/java/se/ju23/typespeeder/logic/TextGenerator.java
+++
@@ -1,5 +0,0 @@
-package se.ju23.typespeeder.logic;
-
-public interface TextGenerator {
-    String generateText();
-}
--- src/main/java/se/ju23/typespeeder/logic/UserInterface.java
+++
@@ -1,6 +0,0 @@
-package se.ju23.typespeeder.logic;
-
-public interface UserInterface {
-    void displayText(String text);
-    String getUserInput();
-}
