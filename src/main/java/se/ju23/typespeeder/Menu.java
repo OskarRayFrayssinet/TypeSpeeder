836297555448691e412c14054e52d28b4db994d2
Emelie Jansson <emelie.jansson@iths.se>
2024-02-20 14:32:26 +0100

    Moved methods into Challenge class

--- src/main/java/se/ju23/typespeeder/Menu.java
+++ src/main/java/se/ju23/typespeeder/Menu.java
@@ -21,16 +21,7 @@ public class Menu implements MenuService {
     private static Object LoggedInUser;
     private static ResourceBundle messages = ResourceBundle.getBundle("Messages");
     static List<String> MenuOptions = new ArrayList<>();
-    public static final String WHITE = "\u001B[37m";
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
     public static Scanner input = new Scanner(System.in);
-    public static boolean stopTimer = false;
-    public static long startTime;
-    public static String game;
-    public static String colorWords;
-    public static List<String> redWords = new ArrayList<>();
 
     public static String userName;
     public static String passWord;
@@ -66,8 +57,8 @@ public class Menu implements MenuService {
         } else {
             switch (menuChoice) {
                 case 0 -> logOut();
-                case 1 -> playGame();
-                case 2 -> showRankingList();
+                case 1 -> Challenge.startChallenge();
+                case 2 -> Challenge.showRankingList();
                 case 3 -> showNewsAndUpdates();
                 case 4 -> changeLanguage();
                 case 5 -> updateUser();
@@ -98,7 +89,6 @@ public class Menu implements MenuService {
         }
     }
 
-
     public static void updateUserProfile(User user) {
         System.out.print("Ange nytt användarnamn (lämna tomt om ingen ändring): ");
         String newUsername = input.nextLine();
@@ -123,96 +113,6 @@ public class Menu implements MenuService {
  //   @Autowired
    // private static UserService userService;
 
-
-    public static void playGame() throws IOException {
-
-       // System.out.println(messages.getString("game.instructions.sv"));
-       // System.out.println(messages.getString("game.instructions"));
-
-        System.out.println(messages.getString("game.instructions"));
-        System.out.println(messages.getString("time.starts"));
-        System.out.print(messages.getString("press.enter.to.play"));
-        input.nextLine();
-        openTextFile();
-        timer();
-        System.out.println();
-        System.out.print(messages.getString("write.here"));
-        game = input.nextLine();
-        stopTimer = true;
-        checkSpelling();
-        checkOrder();
-
-    }
-
-    public static void openTextFile() throws FileNotFoundException {
-        StringBuilder colorWordsBuilder = new StringBuilder();
-        File file = new File("C:\\GitHub Repositories\\TypeSpeeder\\src\\main\\resources\\TextFile");
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String textFile = scanner.nextLine();
-            String[] words = textFile.split(" ");
-            for (String word : words) {
-                String color = Math.random() < 0.5 ? RED : GREEN;
-                colorWordsBuilder.append(color).append(word).append(" ");
-                System.out.print(color + word + " " + RESET);
-            }
-            //System.out.print(RESET);
-        }
-        colorWords = colorWordsBuilder.toString();
-    }
-    public static void timer(){
-    Thread timer = new Thread(()->{
-            startTime = System.currentTimeMillis();
-            while (!stopTimer) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            long timeSeconds = (System.currentTimeMillis() - startTime) / 1000;
-
-            //System.out.println(messages.getString("your.time" + timeSeconds + "seconds"));
-            System.out.println("Din tid blev: " + timeSeconds + " sekunder");
-        });
-        timer.start();
-    }
-    public static void checkSpelling(){
-        String [] words = colorWords.split("\\s+");
-        for (String word : words) {
-            if (word.startsWith(RED)) {
-                redWords.add(word.substring(RED.length()));
-            }
-        }
-        String [] gameList = game.split("\\s+");
-        int countWords = 0;
-        for (String list : gameList){
-            for(String red : redWords){
-                if (red.equals(list)){
-                    countWords++;
-                }
-            }
-        }
-        //System.out.println(messages.getString("correct.words" + countWords));
-        System.out.println("Antal rättstavade ord = " + countWords);
-    }
-    public static void checkOrder(){
-        int countOrder = 0;
-        String[] gameList = game.split("\\s+");
-        int minWord = Math.min(redWords.size(), gameList.length);
-        for(int i = 0; i < minWord; i++){
-            if(gameList[i].equals(redWords.get(i))){
-                countOrder++;
-            } else {
-                break;
-            }
-        }
-        System.out.println("Antal ord i korrekt ordning: " + countOrder);
-    }
-
-    public static void showRankingList() {
-
-    }
 
     public static void showNewsAndUpdates() throws IOException {
         System.out.println("Nyheter och uppdateringar:");
--- src/test/java/se/ju23/typespeeder/ChallengePerformanceTest.java
+++ src/test/java/se/ju23/typespeeder/ChallengePerformanceTest.java
@@ -2,13 +2,16 @@ package se.ju23.typespeeder;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChallengePerformanceTest {
     private static final int MAX_EXECUTION_TIME = 200;
     private static final int MILLISECONDS_CONVERSION = 1_000_000;
     @Test
-    public void testStartChallengePerformance() {
+    public void testStartChallengePerformance() throws IOException {
         Challenge challenge = new Challenge();
         long startTime = System.nanoTime();
         challenge.startChallenge();
