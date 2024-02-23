68567c55e9dc682f0f08a1d39f6b8364e199edef
Husade <119759512+Gazer174@users.noreply.github.com>
2024-02-23 16:33:56 +0100

    small fix with reading from file

--- src/main/java/se/ju23/typespeeder/InfoForUsers/NewsLetter.java
+++ src/main/java/se/ju23/typespeeder/InfoForUsers/NewsLetter.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -32,15 +33,20 @@ public class NewsLetter {
         String text = null;
         Path currentWorkingdir = Paths.get("").toAbsolutePath();
         String path = currentWorkingdir + File.separator + "Newsletter.txt";
+        if (!Files.exists(Paths.get(path))) {
+            createPatchFile(path); // Create the file if it doesn't exist
+        }
         text = readTextFromFile(path);
 
         return text;
     }
-
     public static LocalDateTime getPublishDateTime() {
         LocalDateTime a = null;
         Path currentWorkingdir = Paths.get("").toAbsolutePath();
         String patho = currentWorkingdir + File.separator + "Newsletter.txt";
+        if (!Files.exists(Paths.get(patho))) {
+            createPatchFile(patho); // Create the file if it doesn't exist
+        }
         try {
             Path path = Paths.get(patho);
             BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
@@ -51,6 +57,26 @@ public class NewsLetter {
         }
         return a;
     }
+    private static void createPatchFile(String filePath) {
+        try {
+            Files.createFile(Paths.get(filePath));
+            System.out.println("Created Patch.txt at: " + filePath);
+            // Skriv standardinnehållet till filen
+            String defaultContent = getString();
+            Files.write(Paths.get(filePath), defaultContent.getBytes(), StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            System.err.println("An error occurred while creating the file: " + e.getMessage());
+        }
+    }
+    public static String getString(){
+        return "Welcome to Typespeeder 1.0.1 BETAVERSION.\n" +
+                "Contact our admin to invite some friends.\n" +
+                "To improve our game we would appreciate all the feedback you have!\n" +
+                "\n" +
+                "ENJOY!\n" +
+                "CONTACT Admin: Hussein.adem@iths.se";
+    }
+
     public String readTextFromFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try {
